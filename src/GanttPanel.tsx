72c563e96c7969ad50aef2f797feb273e327a7f2
Marcus Olsson <marcus.olsson@hey.com>
2022-01-05 09:24:40 +0100

    Revert param

--- src/GanttPanel.tsx
+++ src/GanttPanel.tsx
@@ -11,7 +11,7 @@ const usage = {
     { type: FieldType.time, description: 'Task start time' },
     { type: FieldType.time, description: 'Task end time' },
   ],
-  url: 'https://github.com/marcusolsson/grafana-gantt-panel?source=Grafana',
+  url: 'https://github.com/marcusolsson/grafana-gantt-panel',
 };
 
 interface Props extends PanelProps<GanttOptions> {}
